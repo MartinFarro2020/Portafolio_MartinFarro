1b26bf1c6684d0d1fb04d058d3e4340a0665de0c
martinfarro <martin.farro.martinez@gmail.com>
2024-02-12 19:15:34 -0500

    skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,54 +10,54 @@ const Skills = () => {
                 
                 <div className="flex flex-col h-[80px] w-[100px] sm:h-[130px] sm:w-[130px] justify-center items-center mx-auto mt-4 border-solid border-[#9ca1a7] dark:border-[#dfe6e2] border-4 rounded-xl link_gold">
                     <a href="https://www.instagram.com/faromartines/saved/web-developer/18180633910303438/">
-                        <img className="h-[40px]" src="/public/images/js.png" alt="nose" />
+                        <img className="h-[40px]" src="/images/js.png" alt="nose" />
                     </a>
                     
                     <p className="text-[#d6ded9]">JavaScript</p>
                 </div>
     
                 <div className="flex flex-col h-[80px] w-[100px] sm:h-[130px] sm:w-[130px] justify-center items-center mx-auto mt-4 border-solid border-[#9ca1a7] dark:border-[#dfe6e2] border-4 rounded-xl link_gold">
-                  <img className="h-[40px]" src="/public/images/html.png" alt="" />
+                  <img className="h-[40px]" src="/images/html.png" alt="" />
                   <p className="text-[#d6ded9]">HTML</p>
                 </div>
     
                 <div className="flex flex-col h-[80px] w-[100px] sm:h-[130px] sm:w-[130px] justify-center items-center mx-auto mt-4 border-solid border-[#9ca1a7] dark:border-[#dfe6e2] border-4 rounded-xl link_gold">
-                    <img className="h-[40px]" src="/public/images/css.png" alt="" />
+                    <img className="h-[40px]" src="/images/css.png" alt="" />
                     <p className="text-[#d6ded9]">CSS</p>
                 </div>
                     
                 <div className="flex flex-col h-[80px] w-[100px] sm:h-[130px] sm:w-[130px] justify-center items-center mx-auto mt-4 border-solid border-[#9ca1a7] dark:border-[#dfe6e2] border-4 rounded-xl link_gold">
-                    <img className="h-[40px]" src="/public/images/React-icon.png" alt="" />
+                    <img className="h-[40px]" src="/images/React-icon.png" alt="" />
                     <p className="text-[#d6ded9]">REACT</p>
                 </div>
     
                 <div className="flex flex-col h-[80px] w-[100px] sm:h-[130px] sm:w-[130px] justify-center items-center mx-auto mt-4 border-solid border-[#9ca1a7] dark:border-[#dfe6e2] dark:border-[#dfe6e2] border-4 rounded-xl link_gold">
-                    <img className="h-[40px]" src="/public/images/tailwind.png" alt="" />
+                    <img className="h-[40px]" src="/images/tailwind.png" alt="" />
                     <p className="text-[#d6ded9]">Tailwin</p>
                 </div>
     
                 <div className="flex flex-col h-[80px] w-[100px] sm:h-[130px] sm:w-[130px] justify-center items-center mx-auto mt-4 border-solid border-[#9ca1a7] dark:border-[#dfe6e2] border-4 rounded-xl link_gold">
-                    <img className="h-[40px]" src="/public/images/git.png" alt="" />
+                    <img className="h-[40px]" src="/images/git.png" alt="" />
                     <p className="text-[#d6ded9]">Git</p>
                 </div>
                 
                 <div className="flex flex-col h-[80px] w-[100px] sm:h-[130px] sm:w-[130px] justify-center items-center mx-auto mt-4 border-solid border-[#9ca1a7] dark:border-[#dfe6e2] border-4 rounded-xl link_gold">
-                    <img className="h-[40px]" src="/public/images/github.png" alt="" />
+                    <img className="h-[40px]" src="/images/github.png" alt="" />
                     <p className="text-[#d6ded9]">GitHub</p>
                 </div>
     
                 <div className="flex flex-col h-[80px] w-[100px] sm:h-[130px] sm:w-[130px] justify-center items-center mx-auto mt-4 border-solid border-[#9ca1a7] dark:border-[#dfe6e2] border-4 rounded-xl link_gold">
-                    <img className="h-[40px]" src="/public/images/Vitejs-logo.png" alt="" />
+                    <img className="h-[40px]" src="/images/Vitejs-logo.png" alt="" />
                     <p className="text-[#d6ded9]">VITE</p>
                 </div>
     
                 <div className="flex flex-col h-[80px] w-[100px] sm:h-[130px] sm:w-[130px] justify-center items-center mx-auto mt-4 border-solid border-[#9ca1a7] dark:border-[#dfe6e2] border-4 rounded-xl link_gold">
-                    <img className="h-[40px]" src="/public/images/node-js.png" alt="" />
+                    <img className="h-[40px]" src="/images/node-js.png" alt="" />
                     <p className="text-[#d6ded9]">NODE</p>
                 </div>
     
                 <div className="flex flex-col h-[80px] w-[100px] sm:h-[130px] sm:w-[130px] justify-center items-center mx-auto mt-4 border-solid border-[#9ca1a7] dark:border-[#dfe6e2] border-4 rounded-xl link_gold">
-                    <img className="h-[40px]" src="/public/images/docker.png" alt="" />
+                    <img className="h-[40px]" src="/images/docker.png" alt="" />
                     <p className="text-[#d6ded9]">DOCKER</p>
                 </div>
     
